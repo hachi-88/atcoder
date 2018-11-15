a42aed773783cfdf0b29420823347a52796606d9
hachi88 <hachi-88@users.noreply.github.com>
2018-11-15 22:31:06 +0900

    add abc112a

---
+++ abc/112/abc112a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n;
+
+    if (n == 1) {
+        cout << "Hello World" << endl;
+        return 0;
+    }
+
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}
