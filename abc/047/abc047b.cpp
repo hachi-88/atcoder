d40627afd85126c0b4b65801fef9669b0ac26976
hachi88 <hachi-88@users.noreply.github.com>
2018-11-19 23:24:44 +0900

    add abc047b

---
+++ abc/047/abc047b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int w, h, n;
+    cin >> w >> h >> n;
+
+    int x, y, a;
+    int w0 = 0, h0 = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x >> y >> a;
+
+        switch (a) {
+            case 1:
+                w0 = x;
+                break;
+            case 2:
+                w = x;
+                break;
+            case 3:
+                h0 = y;
+                break;
+            case 4:
+                h = y;
+                break;
+        }
+    }
+
+    cout << max(0, w - w0) * max(0, h - h0) << endl;
+
+    return 0;
+}
