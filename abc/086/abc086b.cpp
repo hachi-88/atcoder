02ac4d43982d082b6b83fe59564bc1d9cd09d196
hachi-88 <hachi-88@users.noreply.github.com>
2019-02-08 01:13:02 +0900

    add abc086b

---
+++ abc/086/abc086b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    int c = stoi(a + b);
+
+    for (int i = 1; i * i <= c; i++) {
+        if (i * i == c) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+
+    return 0;
+}
