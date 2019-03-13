a9067caadbd815ea3bc9fdd24094725932013dac
hachi-88 <hachi-88@users.noreply.github.com>
2019-03-13 10:58:21 +0900

    add abc049b

---
+++ abc/049/abc049b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+
+    string s[h];
+
+    for (int i = 0; i < h; i++) {
+        cin >> s[i];
+    }
+
+    for (int i = 0; i < h; i++) {
+        cout << s[i] << endl << s[i] << endl;
+    }
+
+    return 0;
+}
